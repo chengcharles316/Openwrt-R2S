e15b8a258050f5338a9f79462ea1c69006995041
chengcharles316 <67448932+chengcharles316@users.noreply.github.com>
2021-01-25 10:59:14 +0800

    Update 3_prepare_packages.sh

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -10,7 +10,7 @@ if [[ `pwd` == *"rk3328"* ]]; then
 fi
 [ ! -d luci-app-adguardhome ] && git clone https://github.com/rufengsuixing/luci-app-adguardhome
 [ ! -d luci-app-mentohust ] && git clone https://github.com/BoringCat/luci-app-mentohust
-[ ! -d mentohust ] && svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/mentohust
+[ ! -d mentohust ] && svn co https://github.com/1715173329/openwrt/trunk/package/ctcgfw/mentohust
 rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 rm -rf luci-theme-edge/ && git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
 rm -rf luci-theme-infinityfreedom/ && git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
