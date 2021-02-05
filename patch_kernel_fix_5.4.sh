1b8690bf7614526cabd74347491e5ea75adbf1f4
chengcharles316 <67448932+chengcharles316@users.noreply.github.com>
2021-02-05 10:42:47 +0800

    Update patch_kernel_fix_5.4.sh

--- patch_kernel_fix_5.4.sh
+++ patch_kernel_fix_5.4.sh
@@ -4,14 +4,16 @@ git apply ../../add_fullconenat.diff
 #wget https://raw.githubusercontent.com/chengcharles316/Openwrt-R2S/master/003-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
 #git apply ../../003-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
 cd ../
-git clone https://github.com/openwrt/openwrt && cd openwrt/
-git checkout a47279154e08d54df05fa8bf45fe935ebf0df5da
+#git clone https://github.com/openwrt/openwrt && cd openwrt/
+#git checkout a47279154e08d54df05fa8bf45fe935ebf0df5da
+git clone https://github.com/openwrt/openwrt --depth=1 && cd openwrt/
 #rm target/linux/generic/pending-5.4/403-mtd-hook-mtdsplit-to-Kbuild.patch
 #rm target/linux/generic/hack-5.4/700-swconfig_switch_drivers.patch
 cp -a ./target/linux/generic/files/* ../kernel/
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/backport-5.4
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/pending-5.4
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/hack-5.4
+./scripts/patch-kernel.sh ../kernel target/linux/rockchip/patches-5.4
 cd ../
 wget https://github.com/torvalds/linux/raw/master/scripts/kconfig/merge_config.sh && chmod +x merge_config.sh
 grep -i '_NETFILTER_\|FLOW' ../.config.override > .config.override
